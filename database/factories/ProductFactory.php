9e376b1b9b0b245eb44e69302c5d7349178f5f6a
Fabian Suarez <fsuarez120@unab.edu.co>
2026-02-24 15:31:32 -0500

    Creacion de factories

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;    
+
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'description' => fake()->paragraph(),
+            'price'=> fake()->randomFloat(2,100,100000),
+            'category_id' => Category::inRandomOrder()->first()->id
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Product;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,8 +17,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        /*
         $this->call([
             CategorySeeder::class
         ]);
+        */
+
+        Category::factory(100)->create();
+        Product::factory(1000000)->create();
     }
 }
